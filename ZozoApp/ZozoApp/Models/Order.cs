5b1407abcf126939374e63c9b19c6fe09894492f
Moshe Malatji <moshemalatji@gmail.com>
2020-06-22 01:37:06 +0200

    Models Created

--- ZozoApp/ZozoApp/Models/Order.cs
+++ ZozoApp/ZozoApp/Models/Order.cs
@@ -6,10 +6,14 @@ namespace ZozoApp.Models
 {
     public class Order
     {
+        public int id { get; set; }
         public string FullName { get; set; }
-        public string Address { get; set; }
-        public string Phone { get; set; }
-        public int OrderTotal { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public double orderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool isOrderCompleted { get; set; }
         public int UserId { get; set; }
+        public List<OrderDetail> orderDetails { get; set; }
     }
 }
---
+++ ZozoApp/ZozoApp/Services/ApiServices.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using ZozoApp.Models;
+
+namespace ZozoApp.Services
+{
+    public class ApiServices
+    {
+        public static async Task<bool> RegisterUser(string name, string email, string password)
+        {
+            var register = new Register()
+            {
+                Name = name,
+                Email = email,
+                Password = password
+            };
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(register);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("http://localhost:49887/api/Accounts/Register",content);
+            if (!response.IsSuccessStatusCode) return false;
+            return true;
+        }
+
+        public static async Task<bool> Login(string email,string password) 
+        {
+            var login = new Login()
+            {
+                Email = email,
+                Password = password
+            };
+
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(login);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync(ApiSettings.ApiUrl "api/Accounts/Login", content);
+            if (!response.IsSuccessStatusCode) return false;
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Token>(jsonResult);
+            Preferences.Set("accessToken", result.access_token);
+            Preferences.Set("accessToken", result.user_id);
+            Preferences.Set("accessToken", result.user_name);
+            return true;
+        }
+
+        public static async Task<List<bool>> GetCategories()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", Preferences.Get("accessToken", string.Empty));
+                var response = await httpClient.GetStringAsync(AppSettings.ApiUrl + "Api/Categories")
+                return JsonConvert.DeserializeObject<List<Category>>(response);
+        }
+    }
+}
